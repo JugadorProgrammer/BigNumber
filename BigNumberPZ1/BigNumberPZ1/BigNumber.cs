79b2e8a3bcb990abbdfc9c41ebd2a6ff41ad3957
JugadorProgrammer <dinamomoscow2004@gmail.com>
2025-09-19 20:36:22 +0300

    Сделал парсинг строки

--- BigNumberPZ1/BigNumberPZ1/BigNumber.cs
+++ BigNumberPZ1/BigNumberPZ1/BigNumber.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Text;
+using System.Text.RegularExpressions;
 
 namespace BigNumberPZ1
 {
@@ -8,7 +9,7 @@ namespace BigNumberPZ1
         private const char Plus = '+';
         private const char Minus = '-';
         private const int MaxCountOfDigitsInInt = 9;
-        private readonly List<int> _digits;
+        private readonly int[] _digits;
 
         public bool IsNegative { get; }
 
@@ -20,25 +21,58 @@ namespace BigNumberPZ1
             }
 
             var trimmedNumberSpan = number.AsSpan().Trim();
-            if (Regex.IsMatch(trimmedNumberSpan, @"^[-+]?\d+$"))
+            if (!Regex.IsMatch(trimmedNumberSpan, @"^[-+]?\d+$"))
             {
                 throw new ArgumentException("The number must consist of the + and - signs at the beginning and numbers after them!", nameof(number));
             }
 
             IsNegative = (number[0] == Minus) ? true : false;
             var capacity = (int)Math.Ceiling((double)number.Length / MaxCountOfDigitsInInt);
-            _digits = new List<int>(capacity);
+            _digits = new int[capacity];
 
-            for (int i = 0; i < capacity; i++)
+            for (int i = trimmedNumberSpan.Length - 1, counter = capacity - 1;  i >= 0; --counter)
             {
-                var digitSpan = trimmedNumberSpan.Slice((trimmedNumberSpan.Length - 1) * i, MaxCountOfDigitsInInt);
+                var length = Math.Min(MaxCountOfDigitsInInt, i);
+                var start = (length == MaxCountOfDigitsInInt) ? i - MaxCountOfDigitsInInt  : 0;
+
+                var digitSpan = trimmedNumberSpan.Slice(start, length);
                 var digit = int.Parse(digitSpan);
-                _digits.Insert(0, digit);
+                _digits[counter] = digit;
+
+                if (i >= MaxCountOfDigitsInInt)
+                {
+                    i -= MaxCountOfDigitsInInt;
+                }
+                else
+                {
+                    break;
+                }
             }
         }
 
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            if (IsNegative)
+            {
+                builder.Append(Minus);
+            }
+
+            for (int i = 0; i < _digits.Length; ++i)
+            {
+                builder.Append(_digits[i]);
+            }
+            return builder.ToString();
+        }
+
+        public readonly ReadOnlySpan<int> Digits => _digits.AsSpan();
+
         public static BigNumber operator +(BigNumber a, BigNumber b)
         {
+            var aDigits = a.Digits;
+            var bDigits = b.Digits;
+            
+
             return a + b;
         }
 
@@ -59,15 +93,14 @@ namespace BigNumberPZ1
             return a + b;
         }
 
-
         public static BigNumber Pow(BigNumber value, BigNumber degree)
         {
             return value + degree;
         }
 
-        public static BigNumber Pow(BigNumber value, long degree)
-        {
-            return value + degree;
-        }
+        //public static BigNumber Pow(BigNumber value, long degree)
+        //{
+        //    return value + degree;
+        //}
     }
 }
--- BigNumberPZ1/BigNumberPZ1/Program.cs
+++ BigNumberPZ1/BigNumberPZ1/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            var b = new BigNumber("222333444");
         }
     }
 }
